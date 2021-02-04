9c79210ddae3486c35a39b57313de33f6d2ba2c5
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-04 08:36:07 -0600

    added the quote function and file

---
+++ server/modules/qoutes.js
@@ -0,0 +1,21 @@
+// our API is going to GET Quotes
+function getNextQuote() {
+  // retrieve a quote from an array
+
+  const quotes = [
+    {
+      quote:
+        "I'm not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.",
+      author: 'Emma Watson',
+    },
+    {
+      quote:
+        "Remember there's no such thing as a small act of kindness. Every act creates a ripple with no logical end.",
+      author: 'Scott Adams',
+    },
+    {
+      quote: 'Intelligence plus character-that is the goal of true education.',
+      author: 'Martin Luther King, Jr.',
+    },
+  ];
+}
