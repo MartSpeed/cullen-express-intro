190a47ed108a755830ec8fd505af22b49eee4a84
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-04 08:50:19 -0600

    finished settup of the getquote function, setting up get route next

--- server/modules/qoutes.js
+++ server/modules/qoutes.js
@@ -1,8 +1,14 @@
 // our API is going to GET Quotes
+
+// function that takes no arguments
+// instantiates at index 0 which we have set as the staring point
+// the result is set to be the index of the quotes
+// increment the index
+//
 function getNextQuote() {
   // retrieve a quote from an array
-
-  const quotes = [
+  let index = 0;
+  const quotesData = [
     {
       quote:
         "I'm not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.",
@@ -18,4 +24,17 @@ function getNextQuote() {
       author: 'Martin Luther King, Jr.',
     },
   ];
+
+  // put the login in to return a function
+  const result = quotesData[index];
+  //set the index
+  index += 1;
+  //ensures the API always return a value
+  index = index % quotesData.length;
+  // log the result to
+  console.log('this is the result', result);
+  return result;
 }
+
+// a module needs to be imported into another script
+module.exports = getNextQuote;
