41f3adce39ae5eb5677196e45738e2c0f0cc9f3e
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-05 03:25:13 -0600

    missed some coding sturcture, troubleshooting

--- server/modules/qoutes.js
+++ server/modules/qoutes.js
@@ -6,25 +6,24 @@
 // increment the index
 // retrieve a quote from an array
 let index = 0;
+const quotesData = [
+  {
+    quote:
+      "I'm not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.",
+    author: 'Emma Watson',
+  },
+  {
+    quote:
+      "Remember there's no such thing as a small act of kindness. Every act creates a ripple with no logical end.",
+    author: 'Scott Adams',
+  },
+  {
+    quote: 'Intelligence plus character-that is the goal of true education.',
+    author: 'Martin Luther King, Jr.',
+  },
+];
 
 function getNextQuote() {
-  // const quotesData = [
-  //   {
-  //     quote:
-  //       "I'm not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.",
-  //     author: 'Emma Watson',
-  //   },
-  //   {
-  //     quote:
-  //       "Remember there's no such thing as a small act of kindness. Every act creates a ripple with no logical end.",
-  //     author: 'Scott Adams',
-  //   },
-  //   {
-  //     quote: 'Intelligence plus character-that is the goal of true education.',
-  //     author: 'Martin Luther King, Jr.',
-  //   },
-  // ];
-
   // put the quote in to return a function
   const result = quotesData[index];
   //set the quote index
