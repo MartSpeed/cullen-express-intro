857d10f537fe48fa8ec41a17eddd58857be16e84
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-08 08:25:21 -0600

    ajax method breakdown and test

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,6 +7,23 @@ $(document).ready(onReady);
 // run jQuery
 function onReady() {
   console.log('jQuery');
-}
 
-// jQuery functions
+  // jQuery functions
+
+  /**
+   * grab the data from the server
+   *  GET /allTheQuotes
+   *  return an array
+   *  take array of quotes
+   *  loop though them
+   * .append() them to the DOM
+   */
+  let ajaxOptions = {
+    url: '/allTheQuotes',
+    method: 'GET',
+  };
+// set the ajax function using the variable path object
+  $.ajax(ajaxOptions)
+  // using the .then() method to get a response from the GET function
+  .then(function(response) )
+}
