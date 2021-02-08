220a72eaac45d2a0f8440abb019ef8221a66ce6a
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-08 08:55:43 -0600

    appended quotes using the argument to append to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -18,12 +18,28 @@ function onReady() {
    *  loop though them
    * .append() them to the DOM
    */
-  let ajaxOptions = {
+
+  // this is the long hand method to source $.ajax() using a variable method
+  // let ajaxOptions = {
+  //   url: '/allTheQuotes',
+  //   method: 'GET',
+  // };
+
+  // set the ajax function using the variable path object
+  // shorthand $.ajax() will have the variable data put directly into the $.ajax() function call
+  $.ajax({
     url: '/allTheQuotes',
     method: 'GET',
-  };
-// set the ajax function using the variable path object
-  $.ajax(ajaxOptions)
-  // using the .then() method to get a response from the GET function
-  .then(function(response) )
+  })
+    // using the .then() method to get a response from the GET function
+    .then(function (quoteList) {
+      console.log('got a response', quoteList);
+      //let quoteList = response;
+      for (let quote of quoteList) {
+        quote = $('#insert_quote').append(`
+        <li>
+        ${quote.quote}, ${quote.author}
+        </li>`);
+      }
+    });
 }
