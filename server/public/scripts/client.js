bac94a91ed6879daa1efc904dd239240fb922fea
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-08 07:38:37 -0600

    practice with ajax

---
+++ server/public/scripts/client.js
@@ -0,0 +1,12 @@
+// client.js
+console.log('in client.js');
+
+// start jQuery
+$(document).ready(onReady);
+
+// run jQuery
+function onReady() {
+  console.log('jQuery');
+}
+
+// jQuery functions
